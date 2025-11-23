a9ae227bc0ebbf276ed9d2da5884bdf2bd50db7a
anaflaviasfranco <anaflaviasfranco@gmail.com>
2025-11-23 14:39:21 -0300

    feat: set up e home do projeto

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import MovieSection from "./components/MovieSection";
+
+function App() {
+  return (
+    <>
+    <Header />;
+      <Banner
+        src="./banner.png"
+        alt="Banner principal da página do site Luz e Cena"
+      />;
+      <MovieSection />;
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/components/HeaderList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../Header.module.css';
+
+const HeaderList = ({children}: React.HtmlHTMLAttributes<HTMLUListElement>) => {
+  return <ul className={styles.cabecalho}> {children}</ul>;
+
+}
+
+export default HeaderList
